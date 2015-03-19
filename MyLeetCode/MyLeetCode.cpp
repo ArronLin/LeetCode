8b9cc7c233a6e73c77973525c70808466ee76fd9
ArronLin <linmartrix@gmail.com>
2015-03-19 14:00:05 +0800

    Added [Merge k Sorted Lists]

--- MyLeetCode/MyLeetCode.cpp
+++ MyLeetCode/MyLeetCode.cpp
@@ -24,12 +24,44 @@
 #include "../Questions/ValidParentheses/ValidParentheses.cpp"
 #include "../Questions/MergeTwoSortedLists/MergeTwoSortedLists.cpp"
 #include "../Questions/GenerateParentheses/GenerateParentheses.cpp"
+#include "../Questions/MergekSortedLists/MergekSortedLists.cpp"
+
+ListNode* ConstructLinkList(int ary[], int n)
+{
+	ListNode *pHead = NULL;
+	ListNode *pPre = NULL;
+	for(int i = 0; i < n; ++i)
+	{
+		ListNode *pNode = new ListNode(ary[i]);
+		if(!pPre)
+		{
+			pHead = pNode;
+			pPre = pNode;
+		}
+		else
+		{
+			pPre->next = pNode;
+			pPre = pNode;
+		}
+	}
+
+	return pHead;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	GenerateParentheses obj;
-	vector<string> vRet = obj.generateParenthesis(3);
+	MergekSortedLists obj;
+	ListNode *p1, *p2, *p3;
+	p1 = p2 = p3 = NULL;
+	int ary1[] = {-2};
+	p2 = ConstructLinkList(ary1, 1);
+	int ary2[] = {-3,-2,1};
+	p3 = ConstructLinkList(ary2, 3);
+	vector<ListNode*> lists;
+	lists.push_back(p1);lists.push_back(p2);lists.push_back(p3);
 
+	ListNode *pRet = obj.mergeKLists(lists);
+	
 	return 0;
 }
 
---
+++ Questions/MergekSortedLists/MergekSortedLists.cpp
@@ -0,0 +1,51 @@
+#include "common.h"
+
+class MergekSortedLists {
+public:
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if(!l1 && !l2)
+			return NULL;
+		if(!l1)
+			return l2;
+		if(!l2)
+			return l1;
+
+		ListNode *pHead = NULL;
+		if(l1->val < l2->val)
+		{
+			pHead = l1;
+			pHead->next = mergeTwoLists(l1->next, l2);
+		}
+		else
+		{
+			pHead = l2;
+			pHead->next = mergeTwoLists(l1, l2->next);
+		}
+
+		return pHead;
+    }
+
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+		int nSize = lists.size();
+		if(nSize == 0)
+			return NULL;
+
+		int nCurSize = nSize;
+		ListNode *pRet = NULL;
+		while(nCurSize > 1)
+		{
+			int nHalfSize = nCurSize/2;
+			for(int i = 0; i < nHalfSize; ++i)
+			{
+				ListNode *l1 = lists[i];
+				ListNode *l2 = lists[i+nHalfSize];
+				pRet = mergeTwoLists(l1, l2);
+				lists[i] = pRet;
+			}
+
+			nCurSize = nCurSize%2? nHalfSize+1 : nHalfSize;
+		}
+
+		return pRet;
+    }
+};
