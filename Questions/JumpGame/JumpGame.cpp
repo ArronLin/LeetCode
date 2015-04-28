aa6a4b167437b84a2975626f190ddd86f3798132
ArronLin <linmartrix@163.com>
2015-04-28 11:36:47 +0800

    Added [Jump Game] DP version Also added .gitignore into control

--- Questions/JumpGame/JumpGame.cpp
+++ Questions/JumpGame/JumpGame.cpp
@@ -2,7 +2,19 @@
 
 class JumpGame {
 public:
-    bool canJump(vector<int>& nums) {
-        
-    }
+	bool canJump(vector<int>& nums) {
+
+		// DP version
+		// 
+		vector<int> vSteps(n, 0);
+
+		for(int i = 1; i < n; ++i)
+		{
+			vSteps[i] = max(vSteps[i-1], A[i-1]) - 1;
+			if(vSteps[i] < 0)
+				return false;
+		}
+
+		return vSteps[n-1] >= 0;
+	}
 };
