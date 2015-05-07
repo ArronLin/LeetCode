da2fad3711bb59e178034180d109f2d88b4a0517
ArronLin <linmartrix@163.com>
2015-05-07 15:24:20 +0800

    Added [Maximum Subarray] Kadane version

--- Questions/MaximumSubarray/MaximumSubarray.cpp
+++ Questions/MaximumSubarray/MaximumSubarray.cpp
@@ -2,47 +2,33 @@
 
 class MaximumSubarray {
 public:
-	int divide(vector<int> &nums, int nLeft, int nRight)
-	{
-		if (nLeft == nRight)
-		{
-			return nums[nLeft];
-		}
-
-		int nMid = (nLeft+nRight+1)/2;
-		if (nMid == nLeft || nMid == nRight)
-		{
-			return max(nums[nLeft]+nums[nRight], max(nums[nLeft], nums[nRight]));
-		}
-
-		int nLeftMax = divide(nums, nLeft, nMid-1);
-		int nRightMax = divide(nums, nMid+1, nRight);
-
-		int nTemp = nums[nMid];
-		int nMax = nTemp;
-		for (int i = nMid-1; i >= nLeft; i--)
-		{
-			nTemp += nums[i];
-			nMax = nTemp > nMax? nTemp : nMax;
-		}
-
-		nTemp = nMax;
-		for (int i = nMid+1; i <= nRight; i++)
-		{
-			nTemp += nums[i];
-			nMax = nTemp > nMax? nTemp : nMax;
-		}
-
-		return max(nMax, max(nLeftMax, nRightMax));
-	}
-
     int maxSubArray(vector<int>& nums) {
+        int nMax = 0;
 		int nSize = nums.size();
 		if (nSize == 0)
 		{
-			return 0;
+			return nMax;
 		}
 
-		return divide(nums, 0, nSize-1);
+		nMax = nums[0];
+		int nSum = nums[0];
+		for (int i = 1; i < nSize; i++)
+		{
+			if (nSum < 0)
+			{
+				nSum = nums[i];
+			}
+			else
+			{
+				nSum += nums[i];
+			}
+
+			if (nSum > nMax)
+			{
+				nMax = nSum;
+			}
+		}
+
+		return nMax;
     }
 };
